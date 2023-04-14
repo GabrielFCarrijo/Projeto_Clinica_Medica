1ba3c826a5ea57835212dc24e04218350c011abc
Daniel Martins <danieljjmartins@gmail.com>
2023-04-14 20:49:54 +0000

    teste

--- src/server.js
+++ src/server.js
@@ -1,7 +1,14 @@
+// Nota: Para iniciar o servidor: abrir a pasta src no terminal e rodar o comando npm run dev
+
 const express = require('express') // O express permite que não seja necessário reiniciar a aplicação à cada alteração feita
 const app = express()
 const mongoose = require('mongoose') // O mongoose é usado para fazer a conexão no banco de dados
-const Usuario = require('./models/usuario')
+const Usuario = require('../models/usuario')
+
+const path = require('path')
+const hbs = require('hbs')
+const { collection } = require('../models/usuario')
+const templatePath = path.join(__dirname, '../templates')
 
 // Conectando ao banco
 mongoose.connect('mongodb+srv://admin:Pi12345678@medical.b5row3q.mongodb.net/Medical?retryWrites=true&w=majority').then(() => {
@@ -15,10 +22,47 @@ mongoose.connect('mongodb+srv://admin:Pi12345678@medical.b5row3q.mongodb.net/Med
 })
 
 app.use(express.json())
+app.set('view engine', 'hbs')
+app.set("views", templatePath)
+app.use(express.urlencoded({extended:false}))
 
 //rotas
 app.get('/', (req, res) => {
-    res.send('Rota Inicial')
+//    res.send('Rota Inicial')
+    res.render('login')
+})
+
+app.get('/cadastro', (req, res) => {
+        res.render('cadastro')
+    })
+
+// login
+app.post("/login", async(req,res) => {
+    try{
+        const check = await collection.findOne({nome:req.body.nome})
+
+        if(check.senha===req.body.senha){
+            res.render('home')
+        }else{
+            res.send('Senha Incorreta')
+        }
+    } catch(error){
+        res.send('Login inválido')
+    }
+})
+
+app.post('/cadastro', async(req, res) => {
+    try{
+        const usuario = {
+            nome: req.body.nome,
+            senha: req.body.senha
+        }
+        await Usuario.create(req.body)
+        res.render("home")
+    } catch (error){
+        console.log(error.message)
+        res.status(500).json({message: error.message})
+    }
 })
 
 // crud usuário
